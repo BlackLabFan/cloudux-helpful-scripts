104b754e97dbcbd29f31e262e3f90e800eb66579
BlackLabFan <joel.reyeseng@gmail.com>
2025-04-23 19:47:39 -0400

    added useful statistics at the end of the log.

--- dns_latency_test.sh
+++ dns_latency_test.sh
@@ -16,13 +16,23 @@ fqfl="/tmp/dns_latency_test_$date_now$file_ext"
 
 nameserver01=$1
 fqdn_of_a_server=$2
+slow_entries=0
+
+echo "Testing $nameserver01 1000 times with hostname: \
+$fqdn_of_a_server" | tee -a $fqfl
 
 for i in {1..1000}
 do
   echo -n "$(date +%H:%M:%S)  "
-  dig @$nameserver01 $fqdn_of_a_server | grep Query | tr -d ";" | tee -a $fqfl
+  log_entry=`dig @$nameserver01 $fqdn_of_a_server | grep Query | tr -d ";"`
+  echo $log_entry | tee -a $fqfl
+  query_time=`echo $log_entry | awk '$3 >= 30{ print "1" } 
+  $3 <= 29{ print "0" }'`
+#  echo "here is the val of querytime: $query_time"
+  slow_entries=$((query_time+slow_entries))
   sleep 1
 done
 
 echo ""
-echo "Test complete. Check out the log file located at $fqfl"
+echo "Test complete. $slow_entries out of 1000 tests were slow." | tee -a $fqfl
+echo "Check out the log file located at $fqfl"
