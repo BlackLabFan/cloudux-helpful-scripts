e2f274f4c2a6269f0528b676215c7d0de635e343
BlackLabFan <joel.reyeseng@gmail.com>
2025-04-28 16:30:45 -0400

    require root to execute

--- display_pod_warnings.sh
+++ display_pod_warnings.sh
@@ -2,6 +2,13 @@
 # This program takes in one positional argument: the duration to test.
 # ie: 10m 1h. if no duration is supplied 2m is used.
 
+# check for root
+if ! [ $(id -u) -eq 0 ]
+then
+  echo "You forgot to say the magic word... sudo!"
+  exit 1
+fi
+
 if [ -n "$1" ]
 then
   my_range=$1
@@ -11,9 +18,9 @@ fi
 
 error_flag=0
 
-for i in `sudo kubectl get pods | awk '{print $1}'`
+for i in `kubectl get pods | awk '{print $1}'`
 do
-  total_entries="$(sudo kubectl logs $i --since=$my_range | grep -ce WARN -ce ERROR)"
+  total_entries="$(kubectl logs $i --since=$my_range | grep -ce WARN -ce ERROR)"
   echo "FOUND $total_entries ERRORS AND WARNINGS FOR: $i!"
   if [ $total_entries -gt 0 ]
   then
@@ -21,7 +28,7 @@ do
 
     # print out all warning and error entries in the log file
     # may potentially need --all-containers=true
-    sudo kubectl logs $i --since=$my_range | awk '
+    kubectl logs $i --since=$my_range | awk '
     BEGIN{ do_print=0; new_line=1; print "" } /WARN/ || /ERR/{ do_print=1; new_line=1 } 
     /INFO/ && new_line==1{ do_print=0; print ""; new_line=0 } 
     do_print==1{ print $0 } END{ print "All done!"; print "" }'
