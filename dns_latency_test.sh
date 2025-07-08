d7c735adf91c7d09d88c3b73324524ea2df6568d
BlackLabFan <joel.reyeseng@gmail.com>
2025-07-08 15:39:48 -0400

    improvement to exit if query fails

--- dns_latency_test.sh
+++ dns_latency_test.sh
@@ -1,3 +1,4 @@
+
 #/bin/bash
 
 if [ $# -lt 2 ]
@@ -19,8 +20,7 @@ fi
 nameserver01=$1
 fqdn_of_a_server=$2
 slow_entries=0
-
-
+max_slow=0
 
 date_now=$(date +%Y%m%d_%H%M%S)
 file_ext=".log"
@@ -33,8 +33,21 @@ $fqdn_of_a_server" | tee -a $fqfl
 for i in `seq 1 $cycles`
 do
   echo -n "$(date +%H:%M:%S)  " | tee -a $fqfl
-  log_entry=`dig @$nameserver01 $fqdn_of_a_server | grep Query | tr -d ";"`
+  log_entry=`dig @$nameserver01 $fqdn_of_a_server | tr -d ";" | grep Query`
+  if [ ! $? -eq 0 ]
+  then
+    echo "Query of $fqdn_of_a_server from $nameserver01 failed. Exiting." \
+    | tee -a $fqfl
+    exit 1
+  fi
   echo $log_entry | tee -a $fqfl
+
+  latency=`echo $log_entry | awk '{ print $3 }'`
+  if [ $latency -gt $max_slow ]
+  then
+    max_slow=$latency
+  fi
+
   query_time=`echo $log_entry | awk '$3 >= 30{ print "1" } 
   $3 <= 29{ print "0" }'`
   slow_entries=$((query_time+slow_entries))
@@ -43,4 +56,5 @@ done
 
 echo ""
 echo "Test complete. $slow_entries out of $cycles tests were slow." | tee -a $fqfl
+echo "The slowest response found was $max_slow ." | tee -a $fqfl
 echo "Check out the log file located at $fqfl"
